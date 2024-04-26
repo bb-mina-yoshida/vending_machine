9b50c8f9bd5f44359898f8dae96039d3b5e971f5
bb-mina-yoshida <mina.yoshida@bondband.jp>
2024-04-26 22:41:24 +0900

    画面作成

--- resources/views/Purchase/index.blade.php
+++ resources/views/Purchase/index.blade.php
@@ -2,6 +2,6 @@
 
 <div class="back">
     <a class="btn-common" href="/">帰る</a>
-    <a class="stockBtn">在庫補充して</a>
-    <a class="history">購入履歴</a>
+    <a class="btn-common" href="/Stock">在庫補充して</a>
+    <a class="btn-common" href="/BuyHistory">購入履歴</a>
 </div>
